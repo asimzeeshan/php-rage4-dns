12def1c8469cbdb17d1590a4a9fd676f54c89a99
Asim Zeeshan <asim@techbytes.pk>
2012-11-21 19:52:54 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,105 @@
-php-rage4-dns
-=============
+## Rage4 DNS PHP5 class
 
-A PHP wrapper to easily integrate Rage4 DNS service (www.rage4.com)
+This is a PHP5 wrapper to easily integrate Rage4 DNS service (www.rage4.com) easily. There is no official PHP SDK at the moment so this class can help fill the gap in the meantime.
+
+Number of API calls is not limited at the moment hence no mechanism added to track/limit the same.
+
+The methods introduced in this first release are
+- getDomains()
+- createDomain()
+- deleteDomain()
+- importDomain()
+- getRecords()
+- createRecord()
+- updateRecord()
+- deleteRecord()
+
+You can also consult the official documentation avilable at the following URL:
+http://gbshouse.uservoice.com/knowledgebase/articles/109834-rage4-dns-developers-api
+
+Official set of SDKs by Rage4: http://code.google.com/p/rage4-dns-sdk/ (only dotNET available at the moment)
+
+## Configuration
+
+	include("class.rage4.php");
+	// your username (email) and API KEY from Rage4.com
+	// you can get your API KEY from this URL
+	// https://secure.rage4.com/Secure/ShowProfile
+	$r4 = new rage4('you@yourhost.com', 'your-api-key-here');
+
+## Examples
+
+Here are some examples on how to do basic things. Of course you need to configure the class in your app before using the scriptlets below.
+
+### Get all domain names (zones)
+
+	$response = $r4->getDomains();
+	print_r($response);
+    
+### Create a new domain name (zone)
+
+	// createDomain($domain_name, $email)
+	$response = $r4->createDomain('my-domain-name-here.com', 'you@yourhost.com');
+	print_r($response);
+
+### Delete a new domain name (zone)
+
+In this example, 627 is the ID of the domain zone to be deleted. To know the IDs of the domain zones, do $r4->getDomains(); first
+
+	// deleteDomain($domain_id)
+	$response = $r4->deleteDomain(627);
+	print_r($response);
+
+### Import a new domain name (zone)
+
+Note! You need to allow AXFR transfers
+Note! Only regular domains are supported
+        
+	// importDomain($domain)
+	$response = $r4->importDomain('my-domain-name-here.com');
+	print_r($response);
+
+### Get all records of a domain name (zone)
+
+You need to mention the domain ID for which you need to get all records for. Again, if you are unsure please do a $r4->getDomains(); first
+
+	// getRecords($domain_id);
+	$response = $r4->getRecords(55);
+	print_r($response);
+
+### Create a new record for a particular domain name (zone)
+
+	// createRecord($domain_id, $name, $content, $type="TXT", $priority="", $failover="", $failovercontent="")
+	$response = $r4->createRecord(55, 'my-domain-name-here.com', 'ns1.4dns.com', "NS");
+	print_r($response);
+
+### Update an existing record
+
+5555 is the record id that was returned from the function $r4->createRecord()
+
+Note! No domain_name/domain_id is required while updating a record
+Note! There is no way to update the record-type at the moment, so the easy way is to delete the record first and then recreate with new values (if record-type is changed) e.g. from CNAME to TXT etc
+
+	// updateRecord($record_id, $name, $content, $priority="", $failover="", $failovercontent="")
+	$response = $r4->updateRecord(5555, 'my-domain-name-here.com', 'ns1.4dns.com');
+	print_r($response);
+
+### Delete an existing record
+
+	// deleteRecord($record_id)
+	$response = $r4->deleteRecord(5555);
+	print_r($response);
+    
+## TODO / Wish List
+
+- Add support for creating "reverse IPv4 domain", "reverse IPv6 domain", IPv6 and rDNS
+ 
+## Credits
+
+- [Asim Zeeshan](https://github.com/asimzeeshan)
+- Piotr Ginalski from gbshouse.com for tracking down certain issues in my code
+
+#### Special thanks to
+
+- Rage4.com for providing the free beta run and then the free service to me
+- GitHub for providing this amazing free service
---
+++ class.rage4.php
@@ -0,0 +1,389 @@
+<?php
+/*
+Rage4 DNS PHP5 class
+
+This is a PHP5 wrapper to easily integrate Rage4 DNS service
+(www.rage4.com) easily. There is no official PHP SDK at the 
+moment so this class can help fill the gap in the meantime.
+
+Note: Number of API calls is not limited at the moment hence
+      no mechanism added to track/limit the same.
+------------------------------------------------------------
+Author                          : Asim Zeeshan (www.asim.pk)
+Email                           : asim@techbytes.pk
+Twitter                         : @asimzeeshan
+Usage instruction & download    : https://github.com/asimzeeshan/php-rage4-dns
+------------------------------------------------------------
+    */
+
+class rage4 {
+    private $username           = "";
+    private $password           = "";
+    private $valid_record_types = array(1 => "NS", 2 => "A", 3 => "AAAA", 4 => "CNAME", 5 => "MX", 6 => "TXT", 7 => "SRV", 8 => "PTR");
+
+    /*
+        THE CONSTRUCTOR
+        
+        All API calls uses BASIC authentication using user's 
+         - email address as username
+         - Account Key as password
+        
+        Note: Account Key is available in User Profile section of 
+        Rage4 DNS control panel.
+        ------------------------------------------------------------
+        Parameters: $user and $pass
+        
+        */
+    public function __construct($user, $pass) {
+        if (empty($user) || empty($pass)){
+            $this->throwError("Username and Password cannot be empty!");
+        } else if (!empty($user) && !empty($pass)){
+            $this->username = $this->cleanInput($user);
+            $this->password = $this->cleanInput($pass);
+        }
+    }
+    
+    // Internal method
+    // TODO: Instead of printing, I need to "return" back the error messages
+    private function throwError($err) {
+        echo "<b><font color='#FF0000'>Error:</font></b> ".$err;
+        exit;
+    }
+    
+    // Utility functions
+    private function cleanInput($i) {
+        return trim($i);
+    }
+    
+    // Internal method to debug code, I will leave it here for now
+    public function debug() {
+        echo "<br />";
+        echo "Username: ".$this->username."<br />";
+        echo "Password: ".$this->password."<br />";
+        
+    }
+    
+    // Internal method to debug code, I will leave it here for now
+    private function dump($obj) {
+        echo "<br /><pre>";
+        print_r($obj);
+        echo "</pre>";
+    }
+    
+    /*
+        Core function that queries the API and renders results
+        ------------------------------------------------------------
+        Parameters: $method (it includes the method and/or querystring)
+        
+        */
+    private function doQuery($method) {
+        //echo "Trying ... https://secure.rage4.com/rapi/$method <br />";
+        $ch = curl_init("https://secure.rage4.com/rapi/".$method);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->username.":".$this->password);
+        //echo $this->username.":".$this->password."<br />";
+        //echo "HTTPCODE=".$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE)."<br />";
+        $result = curl_exec($ch);
+        //$this->dump($result);
+        //exit;
+        return $result;
+    }
+
+    /*
+        GET DOMAINS
+        Get all domain names in your Rage4.com account
+        ------------------------------------------------------------
+        Parameters: None
+        
+        */
+    public function getDomains() {
+        $response = $this->doQuery("getdomains");
+        $response = json_decode($response, true);
+
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else {
+            return $response;
+        }
+    }
+    
+    /*
+        CREATE A DOMAIN NAME
+        Create a new domain name (zone) in your Rage4.com account
+        ------------------------------------------------------------
+        Parameters: (all required)
+        $name (string) = domain name
+        $email (string) = regular email address of the domain / NOC manager
+        
+        */
+    public function createDomain($domain_name, $email) {
+        if (empty($domain_name) || empty($email)) {
+            $this->throwError("(method: createDomain) Domain name and Email address is required for 'CreateDomain' method");
+        }
+        
+        $response = $this->doQuery("createregulardomain/?name=$domain_name&email=$email");
+        $response = json_decode($response, true);
+        
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else {
+            return $response;
+        }
+    }
+    
+    /*
+        DELETE A DOMAIN NAME
+        Delete a new domain name using its unique identifier in the
+        system. To know the unqiue identifier, GetDomains() must be
+        called first
+        ------------------------------------------------------------
+        Parameters: (all required)
+        $domain_id (int) = domain id
+        
+        */
+    public function deleteDomain($domain_id) {
+        // explicitly typecast into integer
+        $domain_id = (int)$domain_id;
+        
+        if (empty($domain_id)) {
+            $this->throwError("(method: deleteDomain) Domain id must be a number");
+        }
+        
+        $response = $this->doQuery("deletedomain/$domain_id");
+        $response = json_decode($response, true);
+        
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else {
+            return (bool)$response['status'];
+        }
+    }
+
+    /*
+        IMPORT A DOMAIN NAME
+        Importing a domain name including zone data into the system
+
+        Note! You need to allow AXFR transfers
+        Note! Only regular domains are supported
+        ------------------------------------------------------------
+        Parameters: (all required)
+        $domain (string) = domain
+
+        */
+    public function importDomain($domain) {
+        // explicitly typecast into string
+        $domain = (string)$domain;
+        
+        if (empty($domain)) {
+            $this->throwError("(method: importDomain) Domain must be a valid string");
+        }
+        
+        $response = $this->doQuery("importdomain/?name=$domain");
+        $response = json_decode($response, true);
+        
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else {
+            return (bool)$response['status'];
+        }
+    }
+    
+    /*
+        GET RECORDS OF A DOMAIN NAME
+        Get all records (A, AAAA etc) of a particular domain name
+        ------------------------------------------------------------
+        Parameters: (all required)
+        $domain_id (int) = domain id
+        
+        */
+    public function getRecords($domain_id) {
+        // explicitly typecast into integer
+        $domain_id = (int)$domain_id;
+        
+        if (empty($domain_id)) {
+            $this->throwError("(method: getRecords) Domain id must be a number");
+        }
+        
+        $response = $this->doQuery("getrecords/$domain_id");
+        $response = json_decode($response, true);
+        
+        //$this->dump($response);
+        
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else {
+            return $response;
+        }
+    }
+    
+    /*
+        CREATE NEW RECORD
+        Create new record for a specific domain name
+        ------------------------------------------------------------
+        Parameters: (all required except where mentioned)
+        $domain_id (int)            = domain id
+        $name (string)              = name of the record
+        $content (string)           = content of the record
+        $type (string)              = type, should be one of the following 
+                                        1 = NS
+                                        2 = A
+                                        3 = AAAA
+                                        4 = CNAME
+                                        5 = MX
+                                        6 = TXT
+                                        7 = SRV
+                                        8 = PTR
+        $priority (int)             = priority of the record being created [OPTIONAL!!]
+        $failover (bool)            = Failure support? Yes/No
+        $failovercontent (string)   = Failure IP / content
+        
+        */
+    public function createRecord($domain_id, $name, $content, $type="TXT", $priority="", $failover="", $failovercontent="") {
+        // explicitly typecast into required types
+        $domain_id          = (int)$domain_id;
+        $name               = (string)$this->cleanInput($name);
+        $content            = (string)$this->cleanInput($content);
+        $type               = $this->cleanInput($type);
+        $priority           = $this->cleanInput($priority);
+        //$failover           = (bool)$this->cleanInput($failover);
+        $failovercontent    = (string)$this->cleanInput($failovercontent);
+        
+        
+        if (empty($domain_id)) {
+            $this->throwError("(method: createRecord) Domain id must be a number");
+        }
+        if (empty($name)) {
+            $this->throwError("(method: createRecord) Name cannot be empty");
+        }
+        if (empty($content)) {
+            $this->throwError("(method: createRecord) Content cannot be empty");
+        }
+        if ($failover=="") {
+            $failover = "false";
+        } else {
+            $failover = "true";
+        }
+        
+        $query_string = "$domain_id?";
+        if (!empty($name)) {
+            $query_string .= "name=".$name;
+        }
+        if (!empty($content)) {
+            $query_string .= "&content=".$content;
+        }
+        if (in_array($type, $this->valid_record_types)) {
+            $type_id = array_search($type, $this->valid_record_types);
+            $query_string .= "&type=".$type_id;
+        } else {
+            $this->throwError("(method: createRecord) Type must be a valid option from the following NS, MX, A, AAAA, CNAME, TXT, SRV");        
+        }
+        if (!empty($priority)) {
+            $priority = (int)$priority;
+        }
+        $query_string .= "&priority=$priority&failover=$failover&failovercontent=$failovercontent";
+        
+        $response = $this->doQuery("createrecord/$query_string");
+        $response = json_decode($response, true);
+        
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else if (isset($response['status']) && $response['status']!="") {
+            return "Record added with id ".$response['id'];
+        } else {
+            return $response;
+        }
+    }
+    
+    /*
+        UPDATE EXISTING RECORD
+        Update an existing record for a specific domain name (no need to mention domain name)
+        ------------------------------------------------------------
+        Parameters: (all required except where mentioned)
+        $record_id (int)            = record id that you wish to update
+        $name (string)              = name of the record
+        $content (string)           = content of the record
+        $priority (int)             = priority of the record being created [OPTIONAL!!]
+        $failover (bool)            = Failure support? Yes/No
+        $failovercontent (string)   = Failure IP / content
+        
+        */
+    public function updateRecord($record_id, $name, $content, $priority="", $failover="", $failovercontent="") {
+        // explicitly typecast into required types
+        $record_id          = (int)$record_id;
+        $name               = (string)$this->cleanInput($name);
+        $content            = (string)$this->cleanInput($content);
+        $priority           = $this->cleanInput($priority);
+        //$failover           = (bool)$this->cleanInput($failover);
+        $failovercontent    = (string)$this->cleanInput($failovercontent);
+        
+        
+        if (empty($record_id)) {
+            $this->throwError("(method: updateRecord) Record id must be a number");
+        }
+        if (empty($name)) {
+            $this->throwError("(method: updateRecord) Name cannot be empty");
+        }
+        if (empty($content)) {
+            $this->throwError("(method: updateRecord) Content cannot be empty");
+        }
+        if ($failover=="") {
+            $failover = "false";
+        } else {
+            $failover = "true";
+        }
+        
+        $query_string = "$record_id?";
+        if (!empty($name)) {
+            $query_string .= "name=".$name;
+        }
+        if (!empty($content)) {
+            $query_string .= "&content=".$content;
+        }
+        if (!empty($priority)) {
+            $priority = (int)$priority;
+        }
+        $query_string .= "&priority=$priority&failover=$failover&failovercontent=$failovercontent";
+        
+        $response = $this->doQuery("updaterecord/$query_string");
+        $response = json_decode($response, true);
+        
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else if (isset($response['status']) && $response['status']!="") {
+            return "Record updated with id ".$response['id'];
+        } else {
+            return $response;
+        }
+    }
+
+    /*
+        DELETE A RECORD
+        Delete a record in an existing domain name (zone) in your 
+        account
+        ------------------------------------------------------------
+        Parameters: (all required)
+        $record_id (int) = record id
+        
+        */
+    public function deleteRecord($record_id) {
+        // explicitly typecast into integer
+        $record_id = (int)$record_id;
+        
+        if (empty($record_id)) {
+            $this->throwError("(method: deleteRecord) Record id must be a number");
+        }
+        
+        $response = $this->doQuery("deleterecord/$record_id");
+        $response = json_decode($response, true);
+        
+        if (isset($response['error']) && $response['error']!="") {
+            return $response['error'];
+        } else {
+            return $response['status'];
+        }
+    }
+    
+}
